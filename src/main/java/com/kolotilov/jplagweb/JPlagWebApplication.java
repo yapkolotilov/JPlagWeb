25b534d2dab6f06e220fcc6f3d99c64217986301
molodoy <officialmolodoy@gmail.com>
2019-12-01 12:01:56 +0300

    Initial commit

---
+++ src/main/java/com/kolotilov/jplagweb/JPlagWebApplication.java
@@ -0,0 +1,13 @@
+package com.kolotilov.jplagweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPlagWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JPlagWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kolotilov/jplagweb/JPlagWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolotilov.jplagweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JPlagWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
