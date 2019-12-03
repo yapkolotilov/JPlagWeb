0eb510a49e7fd0325d0d231cf912f7a795fa8e5b
yapkolotilov <yaroslavcoworker@gmail.com>
2019-12-03 14:57:03 +0300

    Added MatchPart model

---
+++ src/main/java/com/kolotilov/jplagweb/models/MatchPart.java
@@ -0,0 +1,14 @@
+package com.kolotilov.jplagweb.models;
+
+public class MatchPart extends Match {
+
+    private int matchId;
+
+    public int getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(int matchId) {
+        this.matchId = matchId;
+    }
+}
