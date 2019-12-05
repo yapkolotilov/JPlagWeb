4859a2fba078b82077554e6afb36724c48aa1ed4
Yaroslav <yaroslavattacher@gmail.com>
2019-12-05 21:05:14 +0300

    Finished

--- src/main/java/com/kolotilov/jplagweb/controllers/MatchController.java
+++ src/main/java/com/kolotilov/jplagweb/controllers/MatchController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -35,13 +34,18 @@ public class MatchController extends AbstractController {
         return service.getByTaskId(taskId);
     }
 
+    // return match or matchpart or file
     @GetMapping("/{taskId}/{name}")
     public ResponseEntity<?> getByTaskIdAndName(@PathVariable int taskId, @PathVariable String name) {
         return proceed(() -> {
             try {
-                return service.getByTaskIdAndName(taskId, name);
+                try {
+                    return service.getByTaskIdAndName(taskId, name);
+                } catch (EntityNotFoundException e) {
+                    return matchPartService.getByTaskIdAndName(taskId, name);
+                }
             } catch (EntityNotFoundException e) {
-                return matchPartService.getByTaskIdAndName(taskId, name);
+                return getClass().getResourceAsStream(String.format("src/main/static/%s", name));
             }
         });
     }
@@ -59,26 +63,5 @@ public class MatchController extends AbstractController {
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable int id) {
         return proceed(service::delete, id);
-    }
-
-
-    @GetMapping("/{taskId}/back.gif")
-    public InputStream getBackGif() {
-        return getClass().getResourceAsStream("/static/back.gif");
-    }
-
-    @GetMapping("/{taskId}/forward.gif")
-    public InputStream getForwardGif() {
-        return getClass().getResourceAsStream("/static/forward.gif");
-    }
-
-    @GetMapping("/{taskId}/fields.js")
-    public InputStream getFields() {
-        return getClass().getResourceAsStream("/static/fields.js");
-    }
-
-    @GetMapping("/{taskId}/logo.gif")
-    public InputStream getLogo() {
-        return getClass().getResourceAsStream("/static/logo.gif");
     }
 }
